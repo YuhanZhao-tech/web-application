32e1b61b80fa0345f50890dc93991d94d78a2473
Yuhan Zhao <aidenzhao@hotmail.com>
2024-01-31 01:26:35 -0500

    Added star-sky

---
+++ Star-Sky/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
