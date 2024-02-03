2e3254323d17fa0394f7b0a466e9e19735890142
Yuhan Zhao <aidenzhao@hotmail.com>
2024-02-03 02:20:05 -0500

    more albums

--- Music-Player/public/script.js
+++ Music-Player/public/script.js
@@ -1,9 +1,14 @@
 const nextButton = document.getElementById("next");
+const prevButton = document.getElementById("prev");
 const album2 = document.getElementById("album2")
 const body = document.getElementById("body");
 
 nextButton.addEventListener("click", () => {
-    console.log("Hello");
     album2.style.left = 0;
     body.style.backgroundImage = "url('cover2.jpg')";
+})
+
+prevButton.addEventListener("click", () => {
+    album2.style.left = "400px";
+    body.style.backgroundImage = "url('cover1.jpg')";
 })
