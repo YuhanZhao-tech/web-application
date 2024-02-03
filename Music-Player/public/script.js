f479cf45f26f87abbc48e1eb8592c7872afa62cf
Yuhan Zhao <aidenzhao@hotmail.com>
2024-02-03 18:08:44 -0500

    Added music playing function

--- Music-Player/public/script.js
+++ Music-Player/public/script.js
@@ -1,8 +1,32 @@
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
+const playButton = document.getElementById("play");
 const body = document.getElementById("body");
 var albumElement;
 var currentAlbum = 0;
+var song;
+
+function loadAudio() {
+    switch (currentAlbum) {
+        case 0:
+            song = new Audio("retrowave.mp3");
+            break;
+        case 1:
+            song = new Audio("morning.mp3");
+            break;
+        case 2:
+            song = new Audio("space.mp3");
+            break;
+        case 3:
+            song = new Audio("lofi.mp3");
+            break;
+        default:
+            song = new Audio("retrowave.mp3");
+
+    }
+
+}
+
 
 nextButton.addEventListener("click", () => {
     if (currentAlbum < 3) {
@@ -10,6 +34,7 @@ nextButton.addEventListener("click", () => {
         albumElement = document.getElementById(`album${currentAlbum}`);
         albumElement.style.left = 0;
         body.style.backgroundImage = `url('cover${currentAlbum}.jpg')`;
+        loadAudio();
     }
 })
 
@@ -20,5 +45,13 @@ prevButton.addEventListener("click", () => {
         albumElement.style.left = 400 + "px";
         currentAlbum--;
         body.style.backgroundImage = `url('cover${currentAlbum}.jpg')`;
+        loadAudio();
     }
+})
+
+playButton.addEventListener("click", () => {
+
+    if (!song.paused) { song.pause() }
+    else { song.play() }
+
 })
