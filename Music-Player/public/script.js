ebc795529a8ed2d0e7cc4d3d1a6e6954ec92dd81
Yuhan Zhao <aidenzhao@hotmail.com>
2024-02-03 01:50:14 -0500

    Added animation to music player

--- Music-Player/public/script.js
+++ Music-Player/public/script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const nextButton = document.getElementById("next");
+const album2 = document.getElementById("album2")
+const body = document.getElementById("body");
+
+nextButton.addEventListener("click", () => {
+    console.log("Hello");
+    album2.style.left = 0;
+    body.style.backgroundImage = "url('cover2.jpg')";
+})
